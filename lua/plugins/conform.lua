3601cfcd394ae755848320201c736ede775e9d86
Kristian Berg <kristian.berg@tietoevry.com>
2026-01-09 10:11:13 +0100

    Fix md formatting
    Signed-off-by: Kristian Berg <kristian.berg@tietoevry.com>

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = function()
+      local opts = {
+        formatters_by_ft = {
+          lua = { "stylua" },
+          sh = { "shfmt" },
+          css = { "prettierd" },
+          python = {
+            -- To fix auto-fixable lint errors.
+            "ruff_fix",
+            -- To run the Ruff formatter.
+            "ruff_format",
+            -- To organize the imports.
+            "ruff_organize_imports",
+          },
+          yaml = { "yamlfmt" },
+          -- ["markdown"] = { "markdownlint-cli2" },
+          -- ["markdown.mdx"] = { "markdownlint-cli2" },
+          -- ["markdown"] = { "mdformat", "prettier", "markdownlint-cli2", "markdown-toc" },
+          ["markdown.mdx"] = { "mdformat", "prettier", "markdownlint-cli2", "markdown-toc" },
+          markdown = { "mdformat" },
+          -- json = { "prettierd" },
+          -- jsonc = { "prettierd" },
+        },
+        formatters = {
+          prettier = {
+            prepend_args = {
+              "--prose-wrap",
+              "always",
+              "--tab-width",
+              "4",
+              "--print-width",
+              "80",
+            },
+          },
+          mdformat = {
+            prepend_args = {
+              "--wrap",
+              "80",
+              "--align-semantic-breaks-in-list",
+            },
+          },
+          injected = { options = { ignore_errors = true } },
+        },
+      }
+      return opts
+    end,
+  },
+}
